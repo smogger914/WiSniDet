f22a0357b3cb03aaea4837802b69e6e1f161f8c1
ko <ko@debko.(none)>
2010-02-18 14:56:48 -0800

    Adding required library files , renamed name to pmd

--- src/pmd/pmd.c
+++ src/pmd/pmd.c
@@ -1,23 +1,47 @@
 # include <sys/ioctl.h>
-# include <net/if.h>
 # include <netinet/in.h>
 # include <stdio.h>
 # include <arpa/inet.h>
+
+# include "iwlib.h"
+# include "wireless.h"
+//# include <net/if.h>
 
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
 
-int main () {
+/*
+ *  This array is from iwlib.c
+ */
+const char * const iw_operation_mode[] = { "Auto",
+                                        "Ad-Hoc",
+                                        "Managed",
+                                        "Master",
+                                        "Repeater",
+                                        "Secondary",
+                                        "Monitor",
+                                        "Unknown/bug" };
 
-  char              buf[1024];
-  struct ifconf    ifc;
-  struct ifreq      *ifr;
-  int               s;
-  int               nInterfaces;
-  int               i;
+/*
+ *  Name:         isPromiscMonitor
+ *  Description:  Checks to see if any NIC is in promiscuous
+ *                or monitor mode.
+ *  Return:       >0    True, there exists at least 1 card in promisc/monitor
+ *                0     False
+ */
 
-  struct ifreq      ifr2;
+int isPromiscMonitor() {
+
+  char                    buf[1024];
+  struct ifconf           ifc;
+  struct ifreq            ifr;
+  struct iwreq            iwr;
+  struct wireless_config  info;
+  int                     s;
+  int                     nInterfaces;
+  int                     i;
+  int                     retVal = 0;
 
   /* Get a socket handle */
   s = socket (AF_INET, SOCK_DGRAM, 0);
@@ -30,64 +54,56 @@ int main () {
   ifc.ifc_len = sizeof(buf);
   ifc.ifc_buf = buf;
   if (ioctl (s, SIOCGIFCONF, &ifc) < 0) {
-//  if (ioctl (s, SIOCGIFNAME, &ifc) < 0) {
     perror ("ioctl(SIOCIFCONF)");
     return 1;
   }
 
-  /* Iterate through the list of interfaces */
-  ifr = ifc.ifc_req;
-  nInterfaces = ifc.ifc_len / sizeof(struct ifreq);
-  for (i = 0; i < nInterfaces; i++) {
-    struct ifreq *item = &ifr[i];
-
-    printf ("%s:\tIP\t%s", item->ifr_name,
-            inet_ntoa (((struct sockaddr_in *)&item->ifr_addr)->sin_addr));
-
-    if ( ioctl (s, SIOCGIFFLAGS, (char *) ifr) < 0) {
-      perror ("ioctl(SIOCGIFFLAGS)");
-      return 1;
-    }
-
-    if ((ifr->ifr_flags & IFF_UP) == 1) {
-      printf ("\tLOL?");
-    }
-
-    if ((ifr->ifr_flags & IFF_PROMISC) == 1) {
-      printf ("\tLOL!");
-    }
-
-    printf (",\t%s", item);
-
-    printf ("\n");
-  }
-
-  printf ("\n\n==========\n\n");
-
   for (i = 0; i < 255; i++) {
-    memset (&ifr2, 0, sizeof(ifr2));
-    ifr2.ifr_ifindex = i;
-    if (ioctl (s, SIOCGIFNAME, &ifr2) == -1)
+    memset (&ifr, 0, sizeof(ifr));
+    ifr.ifr_ifindex = i;
+    if (ioctl (s, SIOCGIFNAME, &ifr) == -1)
       continue;
-    if (ioctl (s, SIOCGIFFLAGS, &ifr2) == -1) {
-      fprintf (stderr, "ioctl(SIOCGIFFLAGS) , %s\n", ifr2.ifr_name);
-      continue;
-    }
-    if (! (ifr2.ifr_flags & IFF_UP)) {
+    if (ioctl (s, SIOCGIFFLAGS, &ifr) == -1) {
+      fprintf (stderr, "ioctl(SIOCGIFFLAGS) , %s\n", ifr.ifr_name);
       continue;
     }
-
-    if (ifr2.ifr_flags & IFF_PROMISC) {
-      fprintf (stdout, "%s:\tLOLOLOL\n", ifr2.ifr_name);
-    }
-
-    if (ioctl (s, SIOCGIFADDR, &ifr2) == -1) {
-      fprintf (stderr, "ioctl(SIOCGIFADDR) , %s\n", ifr2.ifr_name);
+    if (! (ifr.ifr_flags & IFF_UP)) {
+      fprintf (stdout, "%s: y u down, charlie brown?\n", ifr.ifr_name);
       continue;
     }
-
-  }
+    // ALLMULTI flag bit set
+    if (ifr.ifr_flags & IFF_ALLMULTI) {
+      //fprintf (stdout, "%s:\tALLMULTI\n", ifr.ifr_name);
+      retVal = 1;
+    }
+    // PROMISC flag bit set 
+    if (ifr.ifr_flags & IFF_PROMISC) {
+      //fprintf (stdout, "%s:\tPROMISC\n", ifr.ifr_name);
+      retVal = 2;
+    }
     
+    if (iw_get_ext (s, ifr.ifr_name, SIOCGIWMODE, &iwr) >= 0) {
+      info.has_mode = 1;
+      if (iwr.u.mode < IW_NUM_OPER_MODE)
+        info.mode = iwr.u.mode;
+      else
+        info.mode = IW_NUM_OPER_MODE;
+      //printf ("Mode: %s\n", iw_operation_mode[info.mode]);
+      //printf ("Monitor is %d\n", info.mode);
+      if (info.mode == 6) { /* Monitor mode */
+        retVal = 3; 
+      } /* IF */
+    } /* IF */
+  } /* FOR */
+    
+  return retVal;
 
+}
+
+int main () {
+
+  int retVal = 0;
+  retVal = isPromiscMonitor();
+  printf ("Result: %d\n", retVal);
   return 0;
 }
