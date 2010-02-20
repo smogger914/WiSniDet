f0ae679cef1129da060660064385bb8621a4ea7b
md <foid@ubuntu.(none)>
2010-02-19 16:41:05 -0800

    working cpumon

--- src/cpumon/cpumon.c
+++ src/cpumon/cpumon.c
@@ -1,71 +1,127 @@
-//# include "cpumon.h"
 #include <stdio.h>
 #include <string.h>
-
-#define BUFSIZE 16
+ 
+#define BUFSIZE 15
+#define TEMPSIZE 5
 #define TENMIN 1
 #define FIVEMIN 2
 #define ONEMIN 3
+ 
 
-
-/*                         variables
-*  
+/* convertToD
+* converts an input string to a double
 */
-FILE * f;
-char cpuload[BUFSIZE];
-int tenMin;
-int fiveMin;
-int oneMin;
+float convertToD(char c[TEMPSIZE]){
+	float d;
+	int i;
+	float temp;
+   int nDecimalDigits;
 
-/*                         loadFile
-*  returns a file given an input string
-*/
-FILE * getLoadFile(){
-	
-	return fopen("/proc/loadavg","r");
+	nDecimalDigits = 1;
+
+	for(i = 0; i < strlen(c); i ++){
+		if(c[i] == '.')
+			i++;
+		temp = c[i] - '0';
+		d += temp / nDecimalDigits;
+		nDecimalDigits *= 10;
+		
+	}
+	return d;
+
 }
 
-/*                         getCpuload
-*  returns the average cpu load for the last 10 min, 5 min, and 1 min
+
+/* seperateString
+* seperates out the values of the given input string
+*/
+int seperateString(char * c, int timeInt){
+
+	char  one[TEMPSIZE];
+	char five[TEMPSIZE];
+	char  ten[TEMPSIZE];
+	int i, z,w, y;
+	float load;
+   w = 0;
+	y = 0;
+	z = 0;
+	
+
+
+	for(i = 0; i < BUFSIZE; i++){
+		if(i < 4){
+				ten[y] = c[i];
+				y++;
+				ten[y] = '\0';
+		}
+		if(i > 4 && i < 9){
+				five[z] = c[i];
+				z++;
+				five[z] = '\0';
+		}
+		if(i > 9 && i < 14){
+				one[w] = c[i];
+				w++;
+				one[w] = '\0';
+		}
+
+	}
+	switch(timeInt){
+ 
+		case TENMIN:
+			load = convertToD(ten);
+			break;
+
+		case FIVEMIN:
+			load = convertToD(five);
+			break;
+		case ONEMIN:
+			load = convertToD(one);
+			break;
+ 
+	}
+	//printf("load: %f\n", load);
+
+	return load;
+}
+/* loadFile
+* returns a file given an input string
+*/
+FILE * getLoadFile(){
+ 
+return fopen("/proc/loadavg","r");
+}
+ 
+/* getCpuload
+* returns the average cpu load for the last 10 min, 5 min, and 1 min
 */
 int getCpuLoad(FILE *f, int t){
+
 	float load;
-	char temp[4];
+	char * temp;
+	int c;
 	if(f == NULL){
 		printf("file not found\n");
 	}
-		fgets(cpuload, BUFSIZE,f);
-		printf("%s\n",cpuload);
+	char cpuload[BUFSIZE];
+	fgets(cpuload, BUFSIZE,f);
 
-	switch(t){
+ 	seperateString(cpuload, 1);
+	
 
-		case TENMIN:
-			strncpy(temp,cpuload, 4);
-				
-			break;
-		case FIVEMIN:
-			printf("fivemin\n");
-			break;
-		case ONEMIN:
-			printf("onemin\n");
-			break;
-
-	}
-	printf("temp: %s \n",temp);
-	printf("load: %f \n",load);
 	return load;
 }
-/*                         main
-*  creates file
-*  getCpuLoad
-* 
+/* main
+* creates file
+* getCpuLoad
+*
 */
 void main(void){
-
+ 
+	FILE * f;
+	char cpuload[BUFSIZE];
 	f = getLoadFile();
 	getCpuLoad(f,TENMIN);
-	
+ 	
 }
-//case statement
-//getrid of printf f make return integer
-//for 1 5 or 10
+
