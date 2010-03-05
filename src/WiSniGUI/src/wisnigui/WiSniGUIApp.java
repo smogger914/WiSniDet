5a6b305750f618a76bdc6e232d7288249336844e
md <foid@ubuntu.(none)>
2010-03-05 14:12:14 -0800

    the actual code

---
+++ src/WiSniGUI/src/wisnigui/WiSniGUIApp.java
@@ -0,0 +1,44 @@
+/*
+ * WiSniGUIApp.java
+ */
+
+package wisnigui;
+
+import org.jdesktop.application.Application;
+import org.jdesktop.application.SingleFrameApplication;
+
+/**
+ * The main class of the application.
+ */
+public class WiSniGUIApp extends SingleFrameApplication {
+
+    /**
+     * At startup create and show the main frame of the application.
+     */
+    @Override protected void startup() {
+        show(new WiSniGUIView(this));
+    }
+
+    /**
+     * This method is to initialize the specified window by injecting resources.
+     * Windows shown in our application come fully initialized from the GUI
+     * builder, so this additional configuration is not needed.
+     */
+    @Override protected void configureWindow(java.awt.Window root) {
+    }
+
+    /**
+     * A convenient static getter for the application instance.
+     * @return the instance of WiSniGUIApp
+     */
+    public static WiSniGUIApp getApplication() {
+        return Application.getInstance(WiSniGUIApp.class);
+    }
+
+    /**
+     * Main method launching the application.
+     */
+    public static void main(String[] args) {
+        launch(WiSniGUIApp.class, args);
+    }
+}
---
+++ src/WiSniGUI/src/wisnigui/WiSniServer.java
@@ -0,0 +1,75 @@
+
+package wisnigui;
+
+import java.net.*;
+import java.io.*;
+import java.util.LinkedList;
+/*
+ * main server class
+ */
+public class WiSniServer implements Runnable{
+        //variables
+DatagramSocket aSocket;
+        static LinkedList clientList = new LinkedList();
+
+        /*
+         * thread stuff for server
+         */
+        public void run(){
+            try{
+ 			aSocket = new DatagramSocket(5316);
+			byte[] buffer = new byte[1000];
+
+			while (true){
+				DatagramPacket request = new DatagramPacket(buffer,buffer.length);
+				aSocket.receive(request);
+                               // System.out.println("got one");
+                                //System.out.println(request.getAddress());
+                                //System.out.println(request.getLength());
+                                addClientToList(request.getAddress());
+				//DatagramPacket reply = new DatagramPacket(d.getData(), d
+				//.getLength(), request.getAddress(), request.getPort());
+				//aSocket.send(reply);
+			}
+		}
+		catch (SocketException e){
+			System.out.println("Socket: " + e.getMessage());
+		}
+		catch (IOException e){
+			System.out.println("IO: " + e.getMessage());
+		}
+		finally{
+			if (aSocket != null)
+				aSocket.close();
+		}
+        }
+        /*
+         *adds client IP to the list of clients
+         */
+        public static void addClientToList(java.net.InetAddress d){
+           // System.out.println(d);
+            //String temp;
+            //temp = d.getHostName();
+
+            clientList.add(d.getHostAddress());
+        }
+        /*
+         * returns the LinkedList of clients
+         */
+        public static LinkedList getClientList(){
+            return clientList;
+        }
+        
+	
+        /*
+         * the actauly server work
+         * waits for a Datagram and adds its sender to the list
+         */
+        WiSniServer(){
+                //System.out.println("starting server");
+		aSocket = null;
+		
+	}
+}
+
+
